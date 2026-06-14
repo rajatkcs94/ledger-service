cd67d7d82f67c3b0826918b154293a58dcf4df78
rajatkcs94 <rajatk.cs94@gmail.com>
2026-06-14 11:57:04 +0530

    added producer and consumer

--- src/main/java/com/proj/ledger_service/LedgerServiceApplication.java
+++ src/main/java/com/proj/ledger_service/LedgerServiceApplication.java
@@ -2,8 +2,10 @@ package com.proj.ledger_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class LedgerServiceApplication {
 
 	public static void main(String[] args) {
